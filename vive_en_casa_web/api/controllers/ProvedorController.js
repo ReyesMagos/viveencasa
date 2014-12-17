c7faa45738236cebecca8b658250b12f64da1af2
oscar david gallon rosero <oscarg798@gmail.com>
2014-12-16 21:00:47 -0500

    provedor api added

---
+++ vive_en_casa_web/api/controllers/ProvedorController.js
@@ -0,0 +1,11 @@
+/**
+ * ProvedorController
+ *
+ * @description :: Server-side logic for managing provedors
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ vive_en_casa_web/api/models/Provedor.js
@@ -0,0 +1,25 @@
+/**
+* Provedor.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+  	codigo:{
+  		type:'integer', 
+  		required:true
+  	}, nombre:{
+  		type:'string',
+  		required:true
+  	}, descripcion:{
+  		type:'string',
+  		required:true
+  	}
+
+  }
+};
+
