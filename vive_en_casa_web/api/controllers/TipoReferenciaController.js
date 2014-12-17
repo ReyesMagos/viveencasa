763fc06a4a8037f2862fdcd7d5d25eb630a809d4
reyesmagos <reyesmagossoft.co@gmail.com>
2014-12-16 20:59:48 -0500

    adicion tiporef

---
+++ vive_en_casa_web/api/controllers/TipoReferenciaController.js
@@ -0,0 +1,11 @@
+/**
+ * TipoReferenciaController
+ *
+ * @description :: Server-side logic for managing tiporeferencias
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
