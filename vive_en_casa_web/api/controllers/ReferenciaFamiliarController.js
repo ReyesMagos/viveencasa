595fdde40ade94a9dff3b86038f2a21aff6a33ba
stalbeal <stalbeal10@gmail.com>
2014-12-18 23:31:16 -0500

    adicion edit-referenciafamiliar

--- vive_en_casa_web/api/controllers/ReferenciaFamiliarController.js
+++ vive_en_casa_web/api/controllers/ReferenciaFamiliarController.js
@@ -40,7 +40,42 @@ module.exports = {
 			}
 			return res.redirect('/referenciafamiliar/new/'+req.param('id'));
 		});
+	},  edit:function  (req,res,next) {
+		
+		ReferenciaFamiliar.findOne(req.param('id') ,function referenciaFounded (err, referencia) {
+			if(err){
+				console.log(err);//
+				next(err);
+				return;
+			}
+			
+			return res.view({referencia:referencia});
+		});
+	}, update: function (req,res,next) {
+		var referencia={
+			nombre:req.param('nombre'),
+			apellido:req.param('apellido'),
+			telefono:req.param('telefono'),
+			direccion:req.param('direccion'),
+			parentezco:req.param('parentezco')
+			
+		}
+		ReferenciaFamiliar.update(req.param('id'), referencia, function referenciaUpdated (err, referenciaa) {
+			if(err){
+				return next(err);
+			}
+			return res.redirect('/cliente/show/'+req.param('propietario'));
+		});
+	}, destroy: function (req,res,next) {
+		ReferenciaFamiliar.destroy(req.param('id'), function referenciaRemoved (err) {
+			if (err) {
+				return next(err);
+			}
+			return res.redirect('/cliente/show/'+req.param('id'));
+		});
+		
 	}
+
 	
 };
 
